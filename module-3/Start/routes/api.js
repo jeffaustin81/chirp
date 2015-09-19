0734acff7b1018b7b16c674f34e80f948f6becbe
Jeff Austin <jeffaustin81@gmail.com>
2015-09-19 02:23:10 -0700

    Made changes to api.js

--- module-3/Start/routes/api.js
+++ module-3/Start/routes/api.js
@@ -3,15 +3,36 @@ var router = express.Router();
 
 router.route('/posts')
 
-    // returns all posts
-    .get(function(req, res) {
-        // temporary solution
-        res.send({message: 'TODO return all posts'});
-    })
-
-    .post(function(req, res) {
-        // temporary solution
-        res.send({message: 'TODO Create a new post'});
+// returns all posts
+.get(function(req, res) {
+    // temporary solution
+    res.send({
+        message: 'TODO return all posts'
     });
+})
+
+.post(function(req, res) {
+    // temporary solution
+    res.send({
+        message: 'TODO Create a new post'
+    });
+});
+
+router.route('/posts/:id')
+
+// returns a particular
+.get(function(req, res) {
+    // temporary solution
+    res.send({
+        message: 'TODO return post with ID' + req.params.id
+    });
+})
+// update particular post
+.post(function(req, res) {
+    // temporary solution
+    res.send({
+        message: 'TODO modify post with ID' + req.params.id
+    });
+});
 
 module.exports = router;
